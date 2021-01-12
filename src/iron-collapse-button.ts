c8922af8daf1a36599c1c8deea30ccc7d362478a
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-01-12 12:58:35 -0300

    fix: add dependency @polymer/iron-flex-layout

--- src/iron-collapse-button.ts
+++ src/iron-collapse-button.ts
@@ -1,4 +1,5 @@
 import { customElement, html, LitElement, property } from 'lit-element';
+import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import "@polymer/iron-iconset-svg/iron-iconset-svg";
 import '@polymer/iron-icon/iron-icon.js';
 import '@polymer/iron-collapse/iron-collapse.js';
